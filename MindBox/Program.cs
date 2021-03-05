f64fa96d75e8973ba2378c7c36b297189ca53c80
danielsmakov <54620863+danielsmakov@users.noreply.github.com>
2021-03-05 22:46:03 +0600

    Add class TriangleCheck with method IsRight with comments

--- MindBox/Program.cs
+++ MindBox/Program.cs
@@ -9,8 +9,12 @@ namespace MindBox
         {
             double areaCircle = CalcArea.Circle(2);
             double areaTriangle = CalcArea.Triangle(5, 5, 5);
+            bool isRight1 = TriangleCheck.IsRight(3, 5, 4);
+            bool isRight2 = TriangleCheck.IsRight(4, 5, 4);
             Console.WriteLine(areaCircle);
             Console.WriteLine(areaTriangle);
+            Console.WriteLine(isRight1);
+            Console.WriteLine(isRight2);
             Console.ReadKey();
         }
     }
---
+++ MindBoxLib/TriangleCheck.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MindBoxLib
+{
+    public class TriangleCheck
+    {
+        public static bool IsRight(double a, double b, double c)
+        {
+            // Данный метод проверяет, является ли треугольник прямоугольным по трем его сторонам, используя теорему Пифагора.
+            if ( (Math.Pow(a, 2) == Math.Pow(b, 2) + Math.Pow(c, 2))  ||  (Math.Pow(b, 2) == Math.Pow(a, 2) + Math.Pow(c, 2))  ||  (Math.Pow(c, 2) == Math.Pow(a, 2) + Math.Pow(b, 2)) )
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
